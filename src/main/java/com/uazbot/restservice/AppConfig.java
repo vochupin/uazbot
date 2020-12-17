0175e7c6d6f8b1f25878853e104b0f10c643656f
Vladimir Chupin <vpchupin@gmail.com>
2020-12-17 20:26:37 +0700

    Запуск бота после старта приложения

--- src/main/java/com/uazbot/restservice/AppConfig.java
+++ src/main/java/com/uazbot/restservice/AppConfig.java
@@ -11,11 +11,33 @@ public class AppConfig {
     @Value( "${jdbc.url}" )
     private String jdbcUrl;
 
+    @Value( "${bot.admin.chat.id}" )
+    private String botAdminChatId;
+
+    @Value( "${bot.token}" )
+    private String botToken;
+
     public String getJdbcUrl() {
         return jdbcUrl;
     }
 
     public void setJdbcUrl(String jdbcUrl) {
         this.jdbcUrl = jdbcUrl;
+    }
+
+    public String getBotAdminChatId() {
+        return botAdminChatId;
+    }
+
+    public void setBotAdminChatId(String botAdminChatId) {
+        this.botAdminChatId = botAdminChatId;
+    }
+
+    public String getBotToken() {
+        return botToken;
+    }
+
+    public void setBotToken(String botToken) {
+        this.botToken = botToken;
     }
 }
--- src/main/java/com/uazbot/restservice/RestServiceApplication.java
+++ src/main/java/com/uazbot/restservice/RestServiceApplication.java
@@ -7,6 +7,8 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 
@@ -15,39 +17,43 @@ public class RestServiceApplication {
     private static final Logger log = Logger.getLogger(RestServiceApplication.class);
     private static final int PRIORITY_FOR_SENDER = 1;
     private static final int PRIORITY_FOR_RECEIVER = 3;
-    private static final String BOT_ADMIN = "";
+
+    @Autowired
+    AppConfig appConfig;
 
     public static void main(String[] args) {
-//        ApiContextInitializer.init();
-//        Bot test_habr_bot = new Bot("uazchatbot", "");
-//
-//        MessageReciever messageReciever = new MessageReciever(test_habr_bot);
-//        MessageSender messageSender = new MessageSender(test_habr_bot);
-//
-//        test_habr_bot.botConnect();
-//
-//        Thread receiver = new Thread(messageReciever);
-//        receiver.setDaemon(true);
-//        receiver.setName("MsgReciever");
-//        receiver.setPriority(PRIORITY_FOR_RECEIVER);
-//        receiver.start();
-//
-//        Thread sender = new Thread(messageSender);
-//        sender.setDaemon(true);
-//        sender.setName("MsgSender");
-//        sender.setPriority(PRIORITY_FOR_SENDER);
-//        sender.start();
-//
-//        sendStartReport(test_habr_bot);
-
         SpringApplication.run(RestServiceApplication.class, args);
     }
 
-    private static void sendStartReport(Bot bot) {
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        ApiContextInitializer.init();
+        Bot test_habr_bot = new Bot("uazchatbot", appConfig.getBotToken());
+
+        MessageReciever messageReciever = new MessageReciever(test_habr_bot);
+        MessageSender messageSender = new MessageSender(test_habr_bot);
+
+        test_habr_bot.botConnect();
+
+        Thread receiver = new Thread(messageReciever);
+        receiver.setDaemon(true);
+        receiver.setName("MsgReciever");
+        receiver.setPriority(PRIORITY_FOR_RECEIVER);
+        receiver.start();
+
+        Thread sender = new Thread(messageSender);
+        sender.setDaemon(true);
+        sender.setName("MsgSender");
+        sender.setPriority(PRIORITY_FOR_SENDER);
+        sender.start();
+
+        sendStartReport(test_habr_bot);
+    }
+
+    private void sendStartReport(Bot bot) {
         SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(BOT_ADMIN);
+        sendMessage.setChatId(appConfig.getBotAdminChatId());
         sendMessage.setText("Запустился");
         bot.sendQueue.add(sendMessage);
     }
-
 }
